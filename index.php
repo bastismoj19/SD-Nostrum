819ee99185cd7638caa656e0ff5333ef875e611a
bastismoj19 <151817191+bastismoj19@users.noreply.github.com>
2024-07-18 19:42:10 +0800

    kapoya bitaw

--- index.php
+++ index.php
@@ -1,1 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nostrum</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
+    <!-- Header Section -->
+    <header class="header">
+        <div class="logo"><img src="assets/images/nostrum.png"></div>
+        <nav class="nav">
+            <a href="#">Lorem</a>
+            <a href="#">Ipsum</a>
+            <a href="#">Dolor</a>
+        </nav>
+        <div class="search-bar">
+                <form action="/" method="GET" class="form">
+                  <input type="search" placeholder="Aperiam" class="search-field" />
+                  <button type="submit" class="search-button">
+                    <img src="assets/images/search.png">
+                  </button>
+                </form>
+        </div>
+    </header>
 
+    <!-- Main Banner Section -->
+    <section class="banner">
+        <img src="assets/images/background.png" alt="">
+        <div>
+            <h1>Sed ut perspiciatis unde omnis <br> iste natus error sit</h1>
+            <p>Nemo enim ipsam voluptatem <br> quia voluptas sit</p>
+            <button class="cta-button">Voluptatibus</button>
+        </div>
+        <div>
+            <img src="assets/images/device_black.png" alt="Phone Image" class="banner-image">
+        </div>
+    </section>
+
+    <!-- Feature Section -->
+    <section class="features">
+        <div id="wifi" class="feature">
+            <img src="assets/images/wifi.png" alt="wifiIcon">
+            <p>Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit</p>
+        </div>
+        <div id="share" class="feature">
+            <img src="assets/images/connect.png" alt="shareIcon">
+            <p>Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit</p>
+        </div>
+        <div class="feature">
+            <img src="assets/images/message.png" alt="lock">
+            <p>Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit</p>
+        </div>
+    </section>
+
+    <!-- Product Section -->
+    <section class="product">
+        <div>
+            <img src="assets/images/iphone7.png" alt="Product Image">
+        </div>
+        <div id="product-details">
+            <h2>At vero eos et accusamus et iusto odio</h2>
+            <p>Nam libero tempore, cum soluta nobis est eligendi optio<br> nihil impedit quo minus id quod maxime placeat facere.</p>
+            <button class="cta-button">Vel illum qui</button>
+        </div>
+    </section>
+
+    <section class="bottomPage">
+        <h2>Ut enim ad minima veniam</h2>
+        <div class="content-wrapper">
+            <div class="left-side">
+                <img src="assets/images/phone.png" alt="black-phone">
+            
+            
+                <div class="additional-info">
+                    
+                    <div class="info">
+                        <img src="assets/images/wifi.png" alt="wifi-icon">
+                        <p>Nam libero tempore, cum <br>soluta nobis est eligendi</p>
+                    </div>
+                
+                    <div class="info">
+                        <img src="assets/images/connect.png" alt="connect-icon">
+                        <p>Nam libero tempore, cum <br> soluta nobis est eligendi</p>
+                    </div>
+                    <div class="info">
+                        <img src="assets/images/message.png" alt="message-icon">
+                        <p>Nam libero tempore, cum <br>soluta nobis est eligendi</p>
+                    </div>
+                </div>
+            </div>
+            <div class="right-side">
+                <div class="form-section">
+                    <div class="form-header">Iste natus</div>
+                    <div class="form-content">
+                        <label for="adipiscing">Adipiscing</label>
+                        <input type="text" id="adipiscing" name="adipiscing">
+                        <label for="commodi">Ex ea commodi</label>
+                        <input type="text" id="commodi" name="commodi">
+                        <button type="submit" class="cta-button">Vel illum qui</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
