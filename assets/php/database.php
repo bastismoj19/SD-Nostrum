9e289f6688f0102d6b22b2697664e70ebc506868
bastismoj19 <151817191+bastismoj19@users.noreply.github.com>
2024-07-29 11:53:22 +0800

    Add files via upload

--- assets/php/database.php
+++ assets/php/database.php
@@ -1,1 +1,15 @@
+<?php
 
+$mysqli = new mysqli("localhost", "root", "", "logindb");
+
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
+} else {
+    echo "Connect na bai";
+}
+
+$query = "SELECT * FROM login WHERE username='$username' AND password='$password'";
+$result = $mysqli->query($query);
+
+$mysqli->close();
+?>
