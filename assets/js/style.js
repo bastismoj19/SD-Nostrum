7bc69a9d8de8e9749fe0b53e4d59c34de3fc0fe7
bastismoj19 <151817191+bastismoj19@users.noreply.github.com>
2024-07-29 11:48:41 +0800

    Add files via upload

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,1 +1,22 @@
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tab-link");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+function IphoneList() {
+    window.open("https://apple.fandom.com/wiki/List_of_iPhone_models", "_blank");
+}
+
+function CpList() {
+    window.open("https://m.gsmarena.com/makers.php3", "_blank");
+}
 
