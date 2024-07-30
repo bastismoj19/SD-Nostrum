3893f3c4f8b5ebdbb78d4987cc782e87c5ecd701
bastismoj19 <151817191+bastismoj19@users.noreply.github.com>
2024-07-30 15:14:36 +0800

    Add files via upload

--- assets/php/database.php
+++ assets/php/database.php
@@ -1,15 +1,35 @@
 <?php
 
-$mysqli = new mysqli("localhost", "root", "", "logindb");
+$mysqli = new mysqli("localhost", "root", "", "nostrum");
 
 if ($mysqli->connect_error) {
     die("Connection failed: " . $mysqli->connect_error);
 } else {
-    echo "Connect na bai";
+    echo "connected successfully";
 }
 
-$query = "SELECT * FROM login WHERE username='$username' AND password='$password'";
+$fullname = $_POST['fullname'];
+$password = $_POST['password'];
+$gmail = $_POST['gmail'];
+
+$query = "INSERT INTO `users`(`fullname`,`password`, `gmail`) VALUES ('$emp_name','$password','$gmail')";
 $result = $mysqli->query($query);
 
+if ($result->num_rows == 1) {
+    $_SESSION['fullname'] = $fullname;
+
+    //get the data from db
+    while ($obj = $result->fetch_object()) {
+        $_SESSION['fullname'] = $obj->fullname;
+    }
+
+    header("Location: page1.php");
+} else {
+    
+  echo "<script>alert('Invalid username or password')</script>";
+
+  header("Location: index.php");
+}   
 $mysqli->close();
 ?>
+
